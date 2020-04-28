1a37dcec8c9636b5001c1d3db94185f08241bf48
Lujiayi <874131637@qq.com>
2020-04-28 20:53:05 +0800

    fix

--- src/main/java/pers/lujiayi/im/ImApplication.java
+++ src/main/java/pers/lujiayi/im/ImApplication.java
@@ -3,6 +3,7 @@ package pers.lujiayi.im;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.context.ConfigurableApplicationContext;
 import pers.lujiayi.im.mapper.ImGroupUserMapper;
 import pers.lujiayi.im.mapper.ImMessageMapper;
@@ -13,7 +14,9 @@ import pers.lujiayi.im.websocket.WebSocketServer;
 public class ImApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(ImApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(ImApplication.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        ConfigurableApplicationContext applicationContext = springApplication.run(args);
         WebSocketServer.setApplicationContext(applicationContext);
     }
 
